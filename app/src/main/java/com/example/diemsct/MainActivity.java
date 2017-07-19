9ceda5d6d9638870fec2df1f197db3c336dcbb36
Mayank Phadke <mayank.phadke1996@gmail.com>
2017-07-19 15:23:38 +0530

    changed contents of new navigation drawer lag persists source of lag: logo

--- app/src/main/java/com/example/diemsct/MainActivity.java
+++ app/src/main/java/com/example/diemsct/MainActivity.java
@@ -68,20 +68,15 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.nav_home) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.nav_about) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.nav_academics) {
 
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.nav_student) {
 
         }
-
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
